4904b7676e841f4def37ebf5d893b0a109e2e65a
SauleNikonte <149366857+SauleNikonte@users.noreply.github.com>
2023-11-29 06:32:04 +0200

    11_28

---
+++ Namu darbas  11_28/ND_11_28scriptas.js
@@ -0,0 +1,40 @@
+let vardass = "Philip Anthony";
+let pavarde = "Hopkins";
+//console.log(vardass.length);
+//console.log(pavarde.length);
+//console.log(vardass.length > pavarde.length);
+if(vardass.length > pavarde.length){
+    console.log (pavarde)
+}
+
+let vardas = "Saule";
+let pavardee = "Nikonte";
+let gimimo_metai = 1987;
+let siu_metu_metai = 2023;
+let amzius = siu_metu_metai - gimimo_metai;
+console.log(`Aš esu ${vardas} ${pavardee}. Man yra ${amzius} metai.`);
+
+let name = "Saule";
+let surname = "Nikonte ";
+let inicialai = name[0] + surname[0];
+console.log(`Jūsų inicialai yra ${inicialai}.`);
+
+let aktoriaus_vardas = "Philip Anthony";
+let aktoriaus_pavarde = "Hopkins";
+let trumpinys = aktoriaus_vardas.slice(-3) + aktoriaus_pavarde.slice(-3);
+console.log(trumpinys);
+//arba aktoriaus_vardas[4 5 6] + aktoriaus_pavarde[4 5 6]
+
+
+
+ let data = new Date (' 2023-11-28 ');
+ let menuo = data.getMonth();
+ console.log(menuo);
+
+ let menesis = ["Sausis","Vasaris","Kovas","Balandis","Gegužė","Birželis","Liepa","Rugpjūtis","Rugsėjis","Spalis","Lapkritis","Gruodis"];
+ let datos = new Date (' 2023-11-28 ');
+ let pavadinimas = menesis[datos.getMonth()];
+ console.log(pavadinimas);
+
+ 
+
---
+++ Namu darbas  11_28/scriptas.js
@@ -0,0 +1,18 @@
+let x = 10;
+
+console.log (x);
+//aritmetiniai operatoriai: + - * / % (liekanos operatorius) //
+
+x = x + 5;
+
+x += 5;
+
+console.log(x);
+
+
+if (x < 5){
+
+
+console.log(typeof x);
+console.log(x);
+}
