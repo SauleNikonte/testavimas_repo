c12eabe16d9624957c8611af91b40b0fb5dfafc2
SauleNikonte <149366857+SauleNikonte@users.noreply.github.com>
2023-12-04 15:00:02 +0200

    12_04

---
+++ klases_darbas12_04/ciklai.js
@@ -0,0 +1,22 @@
+let i = 0;
+
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+
+for (let indeksas = 0; indeksas < 10 ; indeksas++) {
+    if (indeksas % 2 !== 0)
+    {console.log(indeksas)}
+}
+
+for (let indeksas = 100; indeksas < 1000 ; indeksas+=4) {
+    {console.log(indeksas)}
+}
+for (let indeksas = 0; indeksas < 10 ; indeksas++) {
+    {document.write("* * * * * * * * *<br>")}
+}
+
+let n = 100;
+for (let indeksas = 0; indeksas)
